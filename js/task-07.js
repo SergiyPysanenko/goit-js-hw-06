dfe118c11db921a6cbfbbefd395c3bea7432e243
SergiyPysanenko <SergiyPysanenko@users.noreply.github.com>
2023-01-15 20:25:13 +0200

    add 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,6 @@ const slider = document.querySelector("#font-size-control");
 const rangingText = document.querySelector("#text");
 
 slider.addEventListener('input', (event) => {
-    const value = event.currentTarget.value
+const value = event.currentTarget.value
 rangingText.style.fontSize = value + "px"
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onForm);
+
+function onForm(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (!email.value || !password.value) {
+    return alert("Усі поля повинні бути заповнені");
+    };
+    
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
